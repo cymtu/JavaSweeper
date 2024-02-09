8bc5c0ffcbd9e6860d3371b3603209ede95624c5
Alexander Barabanov <cymtu@yandex.ru>
2024-02-10 07:08:06 +0800

    Доработка Основного окна

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,7 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class JavaSweeper extends JFrame {
+
+    private JPanel panel;
     public static void main(String[] args) {
-        new JavaSweeper().setVisible(true);
+        new JavaSweeper();
     }
+
+    private JavaSweeper(){
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
+    }
+
+    private void initFrame() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setVisible(true);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
 }
