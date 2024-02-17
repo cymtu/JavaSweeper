e1a7c1d6b32ea4c0722342d1297f5fc22e65fd4e
Alexander Barabanov <cymtu@yandex.ru>
2024-02-17 08:28:47 +0800

    Мышечный адаптер

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -5,6 +5,8 @@ import sweeper.Ranges;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class JavaSweeper extends JFrame {
 
@@ -36,6 +38,22 @@ public class JavaSweeper extends JFrame {
                 }
             }
         };
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int x = e.getX() / IMAGE_SIZE;
+                int y = e.getY() / IMAGE_SIZE;
+                Coord coord = new Coord(x, y);
+                switch (e.getButton()){
+                    case MouseEvent.BUTTON1: game.pressLeftButton(coord); break;
+                    case MouseEvent.BUTTON3: game.pressRightButton(coord); break;
+                    case MouseEvent.BUTTON2: game.start(); break;
+                }
+                panel.repaint();
+            }
+        });
+
         panel.setPreferredSize(new Dimension(Ranges.getSize().x * IMAGE_SIZE, Ranges.getSize().y * IMAGE_SIZE));
         add(panel);
     }
